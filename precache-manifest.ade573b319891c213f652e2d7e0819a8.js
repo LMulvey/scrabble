5211bcc56c9464eb4b2c4993e6e1dbac6a43878e
Lee Mulvey <lmulvey@me.com>
2018-12-13 20:35:51 -0700

    Updates

---
+++ precache-manifest.ade573b319891c213f652e2d7e0819a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3a71a4635b7f888d26f5",
+    "url": "/static/js/main.3a71a463.chunk.js"
+  },
+  {
+    "revision": "4cd6e4a99e9ae3c78b29",
+    "url": "/static/js/1.4cd6e4a9.chunk.js"
+  },
+  {
+    "revision": "3a71a4635b7f888d26f5",
+    "url": "/static/css/main.dfb8187c.chunk.css"
+  },
+  {
+    "revision": "aea507adc216cb5a81cc8d450e266d65",
+    "url": "/index.html"
+  }
+];
